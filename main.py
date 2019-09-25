36b6152f1794ec5091af14fa6abd72a4d795c44f
Ingrid <ingrid@cc.ci.ufpb.br>
2019-09-25 07:00:29 -0300

    Implementação da nova construção

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ def main():
 	matriz_distancias = [] # Lista com as referências das distâncias
 	
 	dimensao, qtd_veiculos, capacidade, lista_demandas, matriz_distancias = \
-				  funcoesGenericas.le_instancia("instancias_teste/P-n19-k2.txt")
+				  funcoesGenericas.le_instancia("instancias_teste/P-n23-k8.txt")
 	
 	#rota_veiculos = construcao_gulosa_distancia(dimensao, qtd_veiculos, capacidade, lista_demandas, matriz_distancias)
 	rota_veiculos = construcao_gulosa_razao(dimensao, qtd_veiculos, capacidade, lista_demandas, matriz_distancias)
+	#rota_veiculos = construcao_extra(dimensao, qtd_veiculos, capacidade, lista_demandas, matriz_distancias)
 	
-	busca_local(rota_veiculos, lista_demandas, matriz_distancias, capacidade)
+	#busca_local(rota_veiculos, lista_demandas, matriz_distancias, capacidade)
 	
 
 	
@@ -167,7 +168,87 @@ def construcao_gulosa_razao(dimensao, qtd_veiculos, capacidade, lista_demandas, 
 	
 	print(contador_clientes)
 	
+	if contador_clientes < dimensao:
+		rota_veiculos = construcao_extra(dimensao, qtd_veiculos, capacidade, lista_demandas, matriz_distancias)
+	
+	print("Construção gulosa por demanda:  \n")
+	print("Distância inicial: ", funcoesGenericas.total_percorrido(rota_veiculos,matriz_distancias))
+	print(rota_veiculos)
+	print("\n")
+	
+	
+	
+	
 	return rota_veiculos
+	
+	
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+def construcao_extra(dimensao, qtd_veiculos, capacidade, lista_demandas, matriz_distancias):
+	
+	rota_veiculos = [] #Esta será uma lista de rotas, onde cada rota corresponde a rota de um veiculo
+	rota = [] #Esta lista servirá para criar as rotas e adicionar na lista de rotas
+	
+	local_atual = 0 #Vai indicar o cliente onde o veículo se encontra. Inicia no depósito
+	capacidade_atual = 0 #Vai indicar a capacidade do veículo que já foi atendida
+	
+	maior_demanda = 0 #Vai auxiliar a encontrar o cliente de menor distancia, iniciado com infinito para as comparações
+
+	clientes_possiveis = [] #Lista que vai armazenar os clientes possíveis de serem visitados que não estouram a capacidade do veículo
+	
+	clientes_visitados = [] #Essa lista vai armazenar os clientes que já foram visitados
+	clientes_visitados.append(0) #Adiciona o depósito na lista de clientes já visitados, pois os veículos iniciam lá
+	
+	for v in range(qtd_veiculos): #Para cada veículo
+	
+		rota.append(0) #Adiciona o depósito ao início da rota
+	
+		#Esse for e o conteudo dele verificam os clientes que ainda não foram visitados e os que não estouram a capacidade do veículo
+		for cliente in range(dimensao):
+			
+			capacidade_temporaria = capacidade_atual + lista_demandas[cliente]
+			
+			if (cliente not in clientes_visitados) and (capacidade_temporaria <= capacidade):
+				clientes_possiveis.append(cliente)
+		
+		while(len(clientes_possiveis) > 0): #Enquanto existir clientes que podem ser visitados sem que a capacidade do veículo estoure
+			for cliente in clientes_possiveis:
+				if lista_demandas[cliente] > maior_demanda:
+					maior_demanda = lista_demandas[cliente]
+					cliente_escolhido = cliente
+		
+			local_atual = cliente_escolhido #Atualiza o local atual
+			rota.append(local_atual) #Adiciona o local a rota
+			capacidade_atual += lista_demandas[local_atual] #Atualiza a capacidade que o veículo está carregando
+			clientes_visitados.append(local_atual) #Indica o local como um cliente que já foi visitado
+			maior_demanda = 0 
+			clientes_possiveis = []
+			
+			#Esse for e o conteudo dele verificam os clientes que ainda não foram visitados e os que não estouram a capacidade do veículo
+			for cliente in range(dimensao):
+			
+				capacidade_temporaria = capacidade_atual + lista_demandas[cliente]
+			
+				if (cliente not in clientes_visitados) and (capacidade_temporaria <= capacidade):
+					clientes_possiveis.append(cliente)
+					
+		rota.append(0) #Adiciona o depósito ao fim da rota
+		rota_veiculos.append(rota) #Adiciona a rota construída a lista que armazena a rota de cada veículo
+		rota = []
+		local_atual = 0
+		capacidade_atual = 0
+	
+	contador_clientes = 0
+	
+	for i in range(dimensao):
+		if i in clientes_visitados:
+			#print(i)
+			contador_clientes += 1
+	
+	print(contador_clientes)
+	
+	return rota_veiculos
+
+
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def busca_local(rota_veiculos, lista_demandas, matriz_distancias, capacidade):
